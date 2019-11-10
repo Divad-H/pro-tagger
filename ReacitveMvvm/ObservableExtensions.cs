eda7209e96348a849c3394c16831761ebc26be0b
David Hübscher <HuebschersDavid@gmail.com>
2019-11-10 12:05:55 +0100

    Create an Observable extension to filter out null values (IObservable<T?> -> IObservable<T>)

---
+++ ReacitveMvvm/ObservableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace ReacitveMvvm
+{
+    public static class ObservableExtensions
+    {
+        public static IObservable<T> SkipNull<T>(this IObservable<T?> o)
+            where T : class
+        {
+            return o
+                .Where(v => v != null)
+                .Select(v => v ?? throw new InvalidOperationException("Unreachable code reached."));
+        }
+    }
+}

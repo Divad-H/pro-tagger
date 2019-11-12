39958eb0d20d436999291c85bf8127df9aedaf16
David Hübscher <HuebschersDavid@gmail.com>
2019-11-12 01:17:32 +0100

    Create a unit-test for ProcomTagger.RefreshCommand.

---
+++ procom_tagger_test/ProcomTaggerTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using procom_tagger;
+using ReacitveMvvm;
+using ReactiveMvvm;
+using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace procom_tagger_test
+{
+    [TestClass]
+    public class ProcomTaggerTest
+    {
+        class TestSchedulers : ISchedulers
+        {
+            public IScheduler Dispatcher => Scheduler.Immediate;
+        }
+
+        [TestMethod]
+        public async Task RefreshRepositoryTest()
+        {
+            using var _diposables = new CompositeDisposable();
+            using var signal = new SemaphoreSlim(0, 1);
+            using var vm = new ProcomTagger(new TestSchedulers());
+            RepositoryViewModel? repositoryViewModel = null;
+            vm.RepositoryObservable
+                .Subscribe(repoVM =>
+                {
+                    repositoryViewModel = repoVM;
+                    signal.Release();
+                })
+                .DisposeWith(_diposables);
+            await signal.WaitAsync(TimeSpan.FromSeconds(10));
+            vm.RepositoryPath = @"../../";
+            Assert.IsTrue(vm.RefreshCommand.CanExecute(null));
+            vm.RefreshCommand.Execute(null);
+            await signal.WaitAsync(TimeSpan.FromSeconds(10));
+            Assert.IsNotNull(repositoryViewModel);
+        }
+    }
+}

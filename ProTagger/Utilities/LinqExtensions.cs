fbf5797d6b62c5f6301d5620a38f0e770f061ca9
David Hübscher <HuebschersDavid@gmail.com>
2020-02-28 02:28:23 +0100

    LinqExtensions: Create the function GroupAdjacent.

--- ProTagger/Utilities/LinqExtensions.cs
+++ ProTagger/Utilities/LinqExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ProTagger.Utilities
 {
@@ -22,6 +23,31 @@ namespace ProTagger.Utilities
         public static IEnumerable<T> Yield<T>(this T item)
         {
             yield return item;
+        }
+
+        public static IEnumerable<TResult> GroupAdjacent<TResult, T>(this IEnumerable<T> source, Func<T, T, bool> groupPredicate, Func<IEnumerable<T>, TResult> resultSelector)
+        {
+            if (source.Any())
+            {
+                var first = source.First();
+                int groupSize = 1;
+                int groupBegin = 0;
+                foreach (var element in source.Skip(1))
+                {
+                    if (groupPredicate(first, element))
+                    {
+                        groupSize++;
+                    }
+                    else
+                    {
+                        yield return resultSelector(source.Skip(groupBegin).Take(groupSize));
+                        groupBegin += groupSize;
+                        groupSize = 1;
+                    }
+                    first = element;
+                }
+                yield return resultSelector(source.Skip(groupBegin));
+            }
         }
     }
 }

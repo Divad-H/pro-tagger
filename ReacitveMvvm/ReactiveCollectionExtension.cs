3e20af2aa42cafb87aad26dcfd5700ca888dbb82
David Hübscher <HuebschersDavid@gmail.com>
2020-03-16 21:53:03 +0100

    Create a function that creates an observable from INotifyCollectionChanged events.

---
+++ ReacitveMvvm/ReactiveCollectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace ReacitveMvvm
+{
+    public static class ReactiveCollectionExtension
+    {
+        public static IObservable<NotifyCollectionChangedEventArgs> MakeObservable(this INotifyCollectionChanged collection)
+        {
+            return Observable
+                .FromEventPattern<NotifyCollectionChangedEventHandler, NotifyCollectionChangedEventArgs>(
+                    h => collection.CollectionChanged += h,
+                    h => collection.CollectionChanged -= h)
+                .Select(args => args.EventArgs);
+        }
+    }
+}

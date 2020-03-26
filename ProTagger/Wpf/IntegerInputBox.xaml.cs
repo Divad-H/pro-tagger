1b961c1fec8ba6278f5c818855ab4567e5e6f21a
David Hübscher <HuebschersDavid@gmail.com>
2020-03-26 21:32:23 +0100

    Create a simple integer input box.

---
+++ ProTagger/Wpf/IntegerInputBox.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ProTagger.Wpf
+{
+    /// <summary>
+    /// Interaction logic for IntegerInputBox.xaml
+    /// </summary>
+    public partial class IntegerInputBox : UserControl
+    {
+        public IntegerInputBox()
+        {
+            InitializeComponent();
+        }
+    }
+}

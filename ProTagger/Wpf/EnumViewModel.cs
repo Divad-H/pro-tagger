1736f94578039f64cbbda097ef94f0c1348d31c1
David Hübscher <HuebschersDavid@gmail.com>
2020-04-07 15:27:18 +0200

    Create a class EnumViewModel.

---
+++ ProTagger/Wpf/EnumViewModel.cs
@@ -0,0 +1,42 @@
+﻿using ReactiveMvvm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace ProTagger.Wpf
+{
+    public class EnumViewModel<TEnum> : INotifyPropertyChanged where TEnum : Enum
+    {
+        private TEnum _value;
+        public TEnum Value
+        {
+            get => _value;
+            set
+            {
+                if (EqualityComparer<TEnum>.Default.Equals(_value, value))
+                    return;
+                _value = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public List<TEnum> Values { get; }
+
+        public IObservable<TEnum> ValueObservable { get; }
+
+        public EnumViewModel(TEnum value)
+        {
+            _value = value;
+            Values = Enum.GetValues(typeof(TEnum)).Cast<TEnum>().ToList();
+            ValueObservable = this.FromProperty(vm => vm.Value);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ProTaggerTest/Wpf/EnumViewModelTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProTagger.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProTaggerTest.Wpf
+{
+    [TestClass]
+    public class EnumViewModelTest
+    {
+        public enum TestEnum
+        {
+            First,
+            Second,
+            Third,
+        }
+
+        [TestMethod]
+        public void SetValue()
+        {
+            var value = TestEnum.Third;
+            var vm = new EnumViewModel<TestEnum>(TestEnum.First);
+            using var _ = vm.ValueObservable
+                .Subscribe(newVal => value = newVal);
+            Assert.AreEqual(TestEnum.First, value);
+            vm.Value = TestEnum.Second;
+            Assert.AreEqual(TestEnum.Second, value);
+        }
+
+        [TestMethod]
+        public void Values()
+        {
+            var vm = new EnumViewModel<TestEnum>(TestEnum.First);
+            Assert.IsTrue(new List<TestEnum>() { TestEnum.First, TestEnum.Second, TestEnum.Third }
+                .Zip(vm.Values)
+                .All((vals) => vals.First == vals.Second));
+        }
+    }
+}

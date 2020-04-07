a8deb3567e1847eca4323948e455233641f2f19b
David Hübscher <HuebschersDavid@gmail.com>
2020-04-07 15:27:51 +0200

    Split CompareOptions into CompareOptions and SimilarityOptions

--- ProTagger/Configuration/CompareOptionsViewModel.cs
+++ ProTagger/Configuration/CompareOptionsViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using LibGit2Sharp;
+using ProTagger.Wpf;
+using ReacitveMvvm;
 using ReactiveMvvm;
 using System;
 using System.ComponentModel;
@@ -10,44 +12,9 @@ namespace ProTagger.Configuration
     public class CompareOptionsViewModel : INotifyPropertyChanged
     {
         public IObservable<CompareOptions> CompareOptionsObservable { get; }
-
-        private int _contextLines;
-        public int ContextLines
-        {
-            get => _contextLines;
-            set
-            {
-                if (value == _contextLines)
-                    return;
-                _contextLines = value;
-                NotifyPropertyChanged();
-            }
-        }
-        private int _interhunkLines;
-        public int InterhunkLines
-        {
-            get => _interhunkLines;
-            set
-            {
-                if (value == _interhunkLines)
-                    return;
-                _interhunkLines = value;
-                NotifyPropertyChanged();
-            }
-        }
-
-        private DiffAlgorithm _algorithm;
-        public DiffAlgorithm Algorithm
-        {
-            get => _algorithm;
-            set
-            {
-                if (value == _algorithm)
-                    return;
-                _algorithm = value;
-                NotifyPropertyChanged();
-            }
-        }
+        public IntegerInputViewModel ContextLinesInput { get; }
+        public IntegerInputViewModel InterhunkLinesInput { get; }
+        public EnumViewModel<DiffAlgorithm> DiffAlgorithm { get; }
 
         private bool _indentHeuristic;
         public bool IndentHeuristic
@@ -61,30 +28,37 @@ namespace ProTagger.Configuration
                 NotifyPropertyChanged();
             }
         }
-        public CompareOptionsViewModel(CompareOptions compareOptions)
+
+        public SimilarityOptionsViewModel SimilarityOptions { get; }
+
+        public CompareOptionsViewModel(ISchedulers schedulers, CompareOptions compareOptions)
         {
-            _contextLines = compareOptions.ContextLines;
-            _interhunkLines = compareOptions.InterhunkLines;
-            _algorithm = compareOptions.Algorithm;
+            ContextLinesInput = new IntegerInputViewModel(schedulers, compareOptions.ContextLines, 0);
+            InterhunkLinesInput = new IntegerInputViewModel(schedulers, compareOptions.InterhunkLines, 0);
+            DiffAlgorithm = new EnumViewModel<DiffAlgorithm>(compareOptions.Algorithm);
             _indentHeuristic = compareOptions.IndentHeuristic;
+            SimilarityOptions = new SimilarityOptionsViewModel(schedulers, compareOptions.Similarity);
 
-            var contextLinesObservable = this.FromProperty(vm => vm.ContextLines);
-            var interhunkLinesObservable = this.FromProperty(vm => vm.InterhunkLines);
-            var algorithmObservable = this.FromProperty(vm => vm.Algorithm);
-            var indentHeuristicObservable = this.FromProperty(vm => vm.IndentHeuristic);
-
-            CompareOptionsObservable = contextLinesObservable
-                .WithLatestFrom(interhunkLinesObservable, (contextLines, interhunkLines) => new { contextLines, interhunkLines })
-                .WithLatestFrom(algorithmObservable, (data, algorithm) => new { data.contextLines, data.interhunkLines, algorithm })
-                .WithLatestFrom(indentHeuristicObservable, (data, indentHeuristic) => new CompareOptions()
-                {
-                    ContextLines = data.contextLines,
-                    InterhunkLines = data.interhunkLines,
-                    Similarity = compareOptions.Similarity,
-                    IncludeUnmodified = compareOptions.IncludeUnmodified,
-                    Algorithm = data.algorithm,
-                    IndentHeuristic = indentHeuristic,
-                })
+            CompareOptionsObservable = Observable
+                .CombineLatest(ContextLinesInput.ValueObservable,
+                    InterhunkLinesInput.ValueObservable,
+                    DiffAlgorithm.ValueObservable,
+                    this.FromProperty(vm => vm.IndentHeuristic),
+                    SimilarityOptions.SimilarityObservable,
+                      (contextLines, 
+                      interhunkLines, 
+                      algorithm, 
+                      indentHeuristic,
+                      similarityOptions) 
+                    => new CompareOptions()
+                    {
+                        ContextLines = contextLines,
+                        InterhunkLines = interhunkLines,
+                        Similarity = similarityOptions,
+                        IncludeUnmodified = false,
+                        Algorithm = algorithm,
+                        IndentHeuristic = indentHeuristic,
+                    })
                 .StartWith(compareOptions);
         }
 
---
+++ ProTagger/Configuration/SimilarityOptionsViewModel.cs
@@ -0,0 +1,60 @@
+﻿using LibGit2Sharp;
+using ProTagger.Wpf;
+using ReacitveMvvm;
+using System;
+using System.Reactive.Linq;
+
+namespace ProTagger.Configuration
+{
+    public class SimilarityOptionsViewModel
+    {
+        public EnumViewModel<RenameDetectionMode> RenameDetectionMode { get; }
+        public IntegerInputViewModel BreakRewriteThresholdInput { get; }
+        public IntegerInputViewModel CopyThresholdInput { get; }
+        public IntegerInputViewModel RenameFromRewriteThresholdInput { get; }
+        public IntegerInputViewModel RenameLimitInput { get; }
+        public IntegerInputViewModel RenameThresholdInput { get; }
+        public EnumViewModel<WhitespaceMode> WhitespaceMode { get; }
+
+        public IObservable<SimilarityOptions> SimilarityObservable { get; }
+
+        public SimilarityOptionsViewModel(ISchedulers schedulers, SimilarityOptions similarityOptions)
+        {
+            RenameDetectionMode = new EnumViewModel<RenameDetectionMode>(similarityOptions.RenameDetectionMode);
+            BreakRewriteThresholdInput = new IntegerInputViewModel(schedulers, similarityOptions.BreakRewriteThreshold);
+            CopyThresholdInput = new IntegerInputViewModel(schedulers, similarityOptions.CopyThreshold);
+            RenameFromRewriteThresholdInput = new IntegerInputViewModel(schedulers, similarityOptions.RenameFromRewriteThreshold);
+            RenameLimitInput = new IntegerInputViewModel(schedulers, similarityOptions.RenameLimit);
+            RenameThresholdInput = new IntegerInputViewModel(schedulers, similarityOptions.RenameThreshold);
+            WhitespaceMode = new EnumViewModel<WhitespaceMode>(similarityOptions.WhitespaceMode);
+
+            SimilarityObservable = Observable
+                .CombineLatest(
+                    RenameDetectionMode.ValueObservable,
+                    BreakRewriteThresholdInput.ValueObservable,
+                    CopyThresholdInput.ValueObservable,
+                    RenameFromRewriteThresholdInput.ValueObservable,
+                    RenameLimitInput.ValueObservable,
+                    RenameThresholdInput.ValueObservable,
+                    WhitespaceMode.ValueObservable,
+                    (
+                        renameDetectionMode,
+                        breakRewriteThreshold,
+                        copyThreshold,
+                        renameFromRewirteThreshold,
+                        renameLimit,
+                        renameThreshold,
+                        whitespaceMode)
+                            => new SimilarityOptions()
+                            {
+                                RenameDetectionMode = renameDetectionMode,
+                                BreakRewriteThreshold = breakRewriteThreshold,
+                                CopyThreshold = copyThreshold,
+                                RenameFromRewriteThreshold = renameFromRewirteThreshold,
+                                RenameLimit = renameLimit,
+                                RenameThreshold = renameThreshold,
+                                WhitespaceMode = whitespaceMode,
+                            });
+        }
+    }
+}

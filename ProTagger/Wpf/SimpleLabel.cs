c71ee706d0243f9df4085b81a868c0e7d81a8fab
David Hübscher <HuebschersDavid@gmail.com>
2020-04-11 17:55:34 +0200

    Use a simplified label to display changed files.

---
+++ ProTagger/Wpf/SimpleLabel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ProTagger.Wpf
+{
+    /// <summary>
+    /// Simple single line label optimized for performance.
+    /// </summary>
+    public class SimpleLabel : FrameworkElement
+    {
+        /// <summary>
+        /// DependencyProperty for <see cref="Text" /> property.
+        /// </summary>
+        public static readonly DependencyProperty TextProperty =
+                DependencyProperty.Register(
+                        nameof(Text),
+                        typeof(string),
+                        typeof(SimpleLabel),
+                        new FrameworkPropertyMetadata(
+                                string.Empty,
+                                FrameworkPropertyMetadataOptions.AffectsRender,
+                                null,
+                                new CoerceValueCallback(CoerceText)));
+
+        /// <summary>
+        /// The Text property defines the content (text) to be displayed.
+        /// </summary>
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        private static object CoerceText(DependencyObject d, object value)
+           => value ?? string.Empty;
+
+        public Typeface Typeface
+        {
+            get => new Typeface("Consolas");
+            set => throw new NotImplementedException();
+        }
+
+        public double Fontsize
+        {
+            get => 12.0;
+            set => throw new NotImplementedException();
+        }
+
+        public Brush Foreground
+        {
+            get => new SolidColorBrush(Colors.Black);
+            set => throw new NotImplementedException();
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            var formattedText = new FormattedText(
+                Text,
+                CultureInfo.InvariantCulture,
+                FlowDirection.LeftToRight,
+                Typeface,
+                Fontsize,
+                Foreground,
+                VisualTreeHelper.GetDpi(this).PixelsPerDip);
+            drawingContext.DrawText(formattedText, new Point(0, 0));
+        }
+    }
+}

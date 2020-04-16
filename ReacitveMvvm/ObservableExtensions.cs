7384574fca7d25b2010cd5fc98c8da1fc589801e
David Hübscher <HuebschersDavid@gmail.com>
2020-04-16 22:42:14 +0200

    Create a class ReactiveBindingExtension for binding directly to Observables and Observers.

--- ReacitveMvvm/ObservableExtensions.cs
+++ ReacitveMvvm/ObservableExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Windows;
 
 namespace ReacitveMvvm
 {
@@ -8,19 +10,33 @@ namespace ReacitveMvvm
     {
         public static IObservable<T> SkipNull<T>(this IObservable<T?> o)
             where T : class
-        {
 #pragma warning disable CS8619 // Nullability of reference types in value doesn't match target type.
-            return o.Where(v => v != null);
+            => o.Where(v => v != null);
 #pragma warning restore CS8619 // Nullability of reference types in value doesn't match target type.
-        }
 
         public static IObservable<T> SkipNull<T>(this IObservable<T?> o)
             where T : struct
-        {
-            return o.Where(v => v.HasValue)
+            => o.Where(v => v.HasValue)
 #pragma warning disable CS8629 // Nullable value type may be null.
                 .Select(v => v.Value);
 #pragma warning restore CS8629 // Nullable value type may be null.
+
+        /// <summary>
+        /// Creates an Observable from a dependency property of type <see cref="{TProperty}"/>
+        /// </summary>
+        /// <typeparam name="TProperty">Type of the dependency property</typeparam>
+        /// <param name="element">The <see cref="DependencyObject"/> owning the property.</param>
+        /// <param name="property">The <see cref="DependencyProperty"/></param>
+        /// <returns>An observable emitting values when the property changes.</returns>
+        public static IObservable<TProperty> FromDependencyProperty<TProperty>(this DependencyObject element, DependencyProperty property)
+        {
+            var propertyDesc = DependencyPropertyDescriptor.FromProperty(property, element.GetType());
+            return Observable
+                .FromEventPattern(
+                    handler => propertyDesc.AddValueChanged(element, handler),
+                    handler => propertyDesc.RemoveValueChanged(element, handler))
+                .Select(ev => (TProperty)(ev.Sender as DependencyObject ?? throw new ArgumentNullException(nameof(ev.Sender)))
+                    .GetValue(property));
         }
     }
 }
---
+++ ReacitveMvvm/ReactiveBindingExtension.cs
@@ -0,0 +1,195 @@
+﻿using ReactiveMvvm;
+using System;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ReacitveMvvm
+{
+    public class ReactiveBindingExtension : MarkupExtension
+    {
+        public string? Path { get; set; }
+        public BindingMode Mode { get; set; } = BindingMode.Default;
+        public IReactiveConverter? Converter { get; set; }
+
+        public ReactiveBindingExtension() { }
+        public ReactiveBindingExtension(string? path)
+            => Path = path;
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (!(serviceProvider.GetService(typeof(IProvideValueTarget)) is IProvideValueTarget valueProvider)
+                || !(valueProvider.TargetObject is DependencyObject element)
+                || !(valueProvider.TargetProperty is DependencyProperty property))
+                return this;
+
+            var dataContextElement = FindFrameworkElement(element)
+                ?? throw new InvalidOperationException("A ReactiveBinding must be set on a dependency property of a FrameworkElement.");
+
+            var dataContextObservable = Observable
+                .FromEvent<DependencyPropertyChangedEventHandler, DependencyPropertyChangedEventArgs>(
+                    action => (sender, args) => action(args),
+                    handler => dataContextElement.DataContextChanged += handler,
+                    handler => { }/*dataContextElement.DataContextChanged -= handler*/)
+                .Select(args => args.NewValue)
+                .StartWith(dataContextElement?.DataContext);
+
+            var _ = SetupBinding(element, property, dataContextObservable, Converter);
+
+            return property.DefaultMetadata.DefaultValue;
+        }
+
+        public IDisposable SetupBinding(DependencyObject element, DependencyProperty property, IObservable<object?> dataContext, IReactiveConverter? converter)
+        {
+            var disposable = new CompositeDisposable();
+            var path = Path?.Split('.') ?? new string[] { };
+
+            if (Mode == BindingMode.Default)
+                Mode = property.GetMetadata(element) is FrameworkPropertyMetadata metadata && metadata.BindsTwoWayByDefault
+                    ? BindingMode.TwoWay
+                    : BindingMode.OneWay;
+
+            if (Mode == BindingMode.OneWayToSource || Mode == BindingMode.TwoWay)
+            {
+                // It is important that we first subscribe and unsubscribe the observer
+                // in the view model to the dependency property changes, because otherwise
+                // the dependency property will emit an invalid value to the previous
+                // subscriber when the data context changes.
+                var basePath = path.SkipLast(1).ToArray();
+                var propertyObservable = dataContext
+                    .Select(obj => new PathNode(obj, basePath))
+                    .FindProperty() // ViewModel containing the observer/observable (IObservable<IObserver<T>?>)
+                    .Select(vm => ObservableBindingHelpers.GetObject(vm, path.Last()))
+                    .SubscribeObserver(element, property)
+                    .DisposeWith(disposable);
+            }
+            if (Mode == BindingMode.OneTime || Mode == BindingMode.OneWay || Mode == BindingMode.TwoWay)
+            {
+                var valueObservable = dataContext
+                    .Select(obj => new PathNode(obj, path))
+                    .FindProperty();
+                if (Mode == BindingMode.OneTime)
+                    valueObservable = valueObservable.Take(1);
+
+                if (converter != null)
+                    valueObservable = converter.Convert(valueObservable);
+
+                valueObservable
+                    .Subscribe(val => element.SetValue(property,
+                         property.PropertyType.IsAssignableFrom(val?.GetType())
+                             ? val
+                             : property.DefaultMetadata.DefaultValue))
+                    .DisposeWith(disposable);
+            }
+
+            return disposable;
+        }
+
+        private static FrameworkElement? FindFrameworkElement(DependencyObject d)
+        {
+            DependencyObject? current = d;
+            while (!(current is FrameworkElement) && current != null)
+                current = LogicalTreeHelper.GetParent(current);
+            return (FrameworkElement?)current;
+        }
+    }
+
+    struct PathNode
+    {
+        public readonly object? @Object;
+        public readonly string[] RemainingPath;
+
+        public PathNode(object? @object, string[] remainingPath)
+            => (@Object, RemainingPath) = (@object, remainingPath);
+    }
+
+    internal static class ObservableBindingHelpers
+    {
+        public static IObservable<object?> FindProperty(this IObservable<PathNode> obs)
+            => obs
+                .Select(node => node.Object == null || !node.RemainingPath.Any()
+                    ? Observable.Return(node.Object)
+                    : ObjectFromPath(node)
+                        .FindProperty())
+                .Switch();
+
+        private static IObservable<PathNode> ObjectFromPath(PathNode node)
+        {
+            var current = node.Object;
+            foreach (var property in node.RemainingPath.Select((name, i) => new { name, i }))
+            {
+                if (current == null)
+                    break;
+                current = GetObject(current, property.name);
+                if (current != null)
+                {
+                    var obs = CastAsObservable(current);
+                    if (obs != null)
+                        return obs.Select(val => new PathNode(val, node.RemainingPath.Skip(property.i + 1).ToArray()));
+                }
+            }
+            return Observable.Return(new PathNode(current, new string[] { }));
+        }
+
+        public static object? GetObject(object? owner, string name)
+            => owner?.GetType().GetProperty(name)?.GetValue(owner);
+
+        public static Type? GetObservableType(object observable)
+        {
+            return observable
+                .GetType()
+                .GetInterfaces()
+                .FirstOrDefault(type => type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IObservable<>))
+                ?.GetGenericArguments()[0];
+        }
+
+        private static object? CallObservableFunction(string func, params object[] parameters)
+        {
+            var propertyType = GetObservableType(parameters[0]);
+            if (propertyType == null)
+                return null;
+            return typeof(ObservableBindingHelpers)
+                   .GetMethod(func, BindingFlags.NonPublic | BindingFlags.Static)
+                   ?.MakeGenericMethod(propertyType)
+                   .Invoke(null, parameters);
+        }
+
+        private static IObservable<object?>? CastAsObservable(object obj)
+            => (IObservable<object?>?)CallObservableFunction(nameof(CastIntoObjectObservable), obj);
+
+        private static IObservable<object?> CastIntoObjectObservable<T>(IObservable<T> observable)
+            => observable
+                .DistinctUntilChanged()
+                .Select(val => (object?)val);
+
+        public static IDisposable SubscribeObserver(this IObservable<object?> observer,
+            DependencyObject element,
+            DependencyProperty property)
+        {
+            return observer
+                .Select(obs =>
+                {
+                    if (obs == null)
+                        return Disposable.Empty;
+                    var subscription = CallObservableFunction(nameof(SubscribeObserverT), obs, element, property);
+                    return (IDisposable?)subscription ?? Disposable.Empty;
+                })
+                .Scan(new SerialDisposable(),
+                    (serial, subsription) => { serial.Disposable = subsription; return serial; })
+                .Subscribe();
+        }
+
+        private static IDisposable SubscribeObserverT<T>(IObserver<T> observer, DependencyObject element, DependencyProperty property)
+        {
+            if (property.OwnerType.IsAssignableFrom(element.GetType()))
+                return element
+                    .FromDependencyProperty<T>(property)
+                    .Subscribe(observer);
+            return Disposable.Empty;
+        }
+    }
+}

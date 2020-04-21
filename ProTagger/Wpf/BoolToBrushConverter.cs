559571f7e7286b3bb692080fc99496eb45d8fc67
David Hübscher <HuebschersDavid@gmail.com>
2020-04-21 21:46:37 +0200

    Add a converter BoolToBrushConverter.

---
+++ ProTagger/Wpf/BoolToBrushConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ProTagger.Wpf
+{
+    public class BoolToBrushConverter : IValueConverter
+    {
+        public SolidColorBrush? TrueBrush { get; set; }
+        public SolidColorBrush? FalseBrush { get; set; }
+
+        public object? Convert(object value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+                return boolValue ? TrueBrush : FalseBrush;
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProTaggerTest/Wpf/BoolToBrushConverterTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProTagger.Wpf;
+using System.Globalization;
+using System.Reactive.Subjects;
+using System.Windows.Media;
+
+namespace ProTaggerTest.Wpf
+{
+    [TestClass]
+    public class BoolToBrushConverterTest
+    {
+        [TestMethod]
+        public void ConvertsToBrush()
+        {
+            var falseBrush = new SolidColorBrush(Colors.Black);
+            var trueBrush = new SolidColorBrush(Colors.White);
+            var converter = new BoolToBrushConverter() { FalseBrush = falseBrush, TrueBrush = trueBrush };
+            using var source = new BehaviorSubject<bool?>(null);
+            var convertedBrush = (Brush?)converter.Convert(false, typeof(Brush), null, CultureInfo.InvariantCulture);
+            Assert.AreEqual(falseBrush, convertedBrush);
+            convertedBrush = (Brush?)converter.Convert(true, typeof(Brush), null, CultureInfo.InvariantCulture);
+            Assert.AreEqual(trueBrush, convertedBrush);
+        }
+    }
+}

25d682c25d218d9654636ad1541b56e9013bab23
David Hübscher <HuebschersDavid@gmail.com>
2020-06-30 05:53:01 +0200

    Create a test that ensures that RepositoryViewModel can be created.

---
+++ ProTaggerTest/Repository/RepositoryViewModelTest.cs
@@ -0,0 +1,31 @@
+﻿using LibGit2Sharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProTagger;
+using ProTagger.RepositorySelection;
+using ProTaggerTest.Mocks;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProTaggerTest.Repository
+{
+    [TestClass]
+    public class RepositoryViewModelTest
+    {
+        [TestMethod]
+        public async Task CanCreateRepositoryViewModel()
+        {
+            var schedulers = new TestSchedulers();
+            var repositoryFactory = new TestRepositoryFactory();
+            var repositoryDescription = new RepositoryDescription("Name", "PATH TO REPO");
+            var res = await RepositoryViewModel.Create(
+                schedulers,
+                CancellationToken.None,
+                repositoryFactory,
+                repositoryDescription,
+                Observable.Return(new CompareOptions()));
+            Assert.IsTrue(res.Is<RepositoryViewModel>());
+            using var repo = res.Get<RepositoryViewModel>();
+        }
+    }
+}

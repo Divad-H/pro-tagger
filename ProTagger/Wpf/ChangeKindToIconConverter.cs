a2cab1f0fe1f903ca5012d7f4695feef0d78445f
David Hübscher <HuebschersDavid@gmail.com>
2020-07-21 00:42:01 +0200

    Add an icon to the tree diff entries.

---
+++ ProTagger/Wpf/ChangeKindToIconConverter.cs
@@ -0,0 +1,35 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ProTagger.Wpf
+{
+    class ChangeKindToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is ChangeKind changeKind))
+                return DependencyProperty.UnsetValue;
+            return changeKind switch
+            {
+                ChangeKind.Unmodified => "\uE73E",
+                ChangeKind.Added => "\uE948",
+                ChangeKind.Deleted => "\uE74D",
+                ChangeKind.Modified => "\uEB7E",
+                ChangeKind.Renamed => "\uE8AC",
+                ChangeKind.Copied => "\uE8C8",
+                ChangeKind.Ignored => "\uE7B3",
+                ChangeKind.Untracked => "\uF142",
+                ChangeKind.TypeChanged => "\uE8AB",
+                ChangeKind.Unreadable => "\uE7BA",
+                ChangeKind.Conflicted => "\uE945",
+                _ => throw new ArgumentOutOfRangeException(nameof(value)),
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}

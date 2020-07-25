d1c8fcd8c157ee2be444e4f74acdea1923f7884b
David Hübscher <HuebschersDavid@gmail.com>
2020-07-25 11:58:22 +0200

    Rename NegationConverter into InversionConverter.

--- ProTagger/Wpf/InversionConverter.cs
+++ ProTagger/Wpf/InversionConverter.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Text;
 using System.Windows;
 using System.Windows.Data;
 
 namespace ProTagger.Wpf
 {
-    class NegationConverter : IValueConverter
+    class InversionConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
